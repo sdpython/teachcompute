186d238ea58fb3fe02cad0908d66ab3a7826e79d
Xavier Dupre <xadupre@microsoft.com>
2023-12-28 01:19:49 +0100

    add cpp files

---
+++ teachcompute/validation/cuda/cuda_tensor.cuh
@@ -0,0 +1,50 @@
+#pragma once
+#include "cuda_nvtx.cuh"
+#include "cuda_utils.h"
+#include <cublasLt.h>
+#include <cublas_v2.h>
+#include <cuda_runtime.h>
+#include <string>
+
+namespace cuda_example {
+
+typedef enum TensorDevice { CPU = 0, CUDA = 1 } TensorDevice;
+
+int32_t type_size(cudaDataType_t element_type);
+
+inline cudaDataType_t get_cuda_dtype(cudaDataType_t dtype) { return dtype; }
+
+struct TensorData {
+  TensorDevice device;
+  cudaDataType_t dtype;
+  std::size_t size;
+  void *dptr;
+  inline TensorData() {
+    device = TensorDevice::CPU;
+    size = 0;
+    dptr = nullptr;
+    dtype = CUDA_R_32F;
+  }
+  void allocate(cudaDataType_t dtype, std::size_t size, TensorDevice device);
+  void free();
+  void copy_from_cpu(void *ptr);
+};
+
+class Tensor {
+public:
+  const char *name;
+  TensorData data;
+  TensorData scale;
+  TensorData amax;
+  TensorData scale_inv;
+
+public:
+  inline Tensor(const char *name) : data(), scale(), amax(), scale_inv() { this->name = name; }
+  Tensor(const char *name, std::size_t size, cudaDataType_t dtype = CUDA_R_32F,
+         TensorDevice device = TensorDevice::CUDA,
+         TensorDevice scale_device = TensorDevice::CUDA);
+  ~Tensor();
+  void rnd();
+};
+
+} // namespace cuda_example
