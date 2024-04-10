53dba6372731fca76b128ef55dfdfb4b57cafabf
Xavier Dupre <xadupre@microsoft.com>
2024-04-10 12:20:26 +0200

    blog

--- _unittests/ut_validation/test_dot_omp.py
+++ _unittests/ut_validation/test_dot_omp.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy
-from teachcompute.ext_test_case import ExtTestCase
+from teachcompute.ext_test_case import ExtTestCase, skipif_ci_apple
 from teachcompute.validation.cython.dot_cython_omp import (
     ddot_cython_array_omp,
     ddot_array_openmp,
@@ -21,6 +21,7 @@ class TestTutorialDotOmp(ExtTestCase):
         res2 = ddot_cython_array_omp(va, vb, schedule=2)
         self.assertTrue(abs(res1 - res2) <= 1e-12)
 
+    @skipif_ci_apple("crash")
     def test_ddot_array_openmp(self):
         va = numpy.random.randn(100).astype(numpy.float64)
         vb = numpy.random.randn(100).astype(numpy.float64)
@@ -28,6 +29,7 @@ class TestTutorialDotOmp(ExtTestCase):
         res2 = ddot_array_openmp(va, vb)
         self.assertTrue(abs(res1 - res2) <= 1e-12)
 
+    @skipif_ci_apple("crash")
     def test_ddot_array_openmp_16(self):
         va = numpy.random.randn(100).astype(numpy.float64)
         vb = numpy.random.randn(100).astype(numpy.float64)
@@ -35,6 +37,7 @@ class TestTutorialDotOmp(ExtTestCase):
         res2 = ddot_array_openmp_16(va, vb)
         self.assertTrue(abs(res1 - res2) <= 1e-12)
 
+    @skipif_ci_apple("crash")
     def test_get_omp_max_threads(self):
         res2 = get_omp_max_threads()
         self.assertTrue(res2 > 0)
