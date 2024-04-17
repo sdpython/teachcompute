ddbb6b71c453a62a419ad1a6797ba782e4646973
xdupre <xav.dupre@ensae.fr>
2024-04-17 10:10:29 +0000

    fix build

--- _unittests/ut_fctmr/test_pyparallel_fctmr.py
+++ _unittests/ut_fctmr/test_pyparallel_fctmr.py
@@ -1,9 +1,10 @@
 import unittest
-from teachcompute.ext_test_case import ExtTestCase
+from teachcompute.ext_test_case import ExtTestCase, skipci_if_apple
 from teachcompute.fctmr.pyparallel_fctmr import pyparallel_mapper
 
 
 class TestPyParallelFctMr(ExtTestCase):
+    @skipci_if_apple("crash")
     def test_pyparallel_mapper(self):
         res = pyparallel_mapper(lambda x: x + 1, [4, 5])
         self.assertNotIsInstance(res, list)
