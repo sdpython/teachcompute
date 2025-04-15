d35c582043efd91d9d4ed596537f18ac7a305b2d
xadupre <xadupre@microsoft.com>
2025-04-16 00:10:16 +0200

    tuto2

--- _tutoriels/cython3/setup.py
+++ _tutoriels/cython3/setup.py
@@ -6,9 +6,11 @@ ext = Extension(
     name="dotcy",
     sources=["dotcy.pyx"],
     include_dirs=[numpy.get_include()],
+    language="c++",
 )
 
 setup(
     name="dotcy",
     ext_modules=cythonize([ext], language_level="3"),
+    define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
 )
